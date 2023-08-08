75e669c5dd5e02574a6a2f81cdc73735a3dc2e6d
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-08 09:04:16 -0300

    .

--- ejemplo-reactnative/App.js
+++ ejemplo-reactnative/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import {useState} from 'react'
-import { ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, View, Button, Alert,TouchableOpacity,Pressable} from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet, Text, TextInput, View, Button, Alert, TouchableOpacity, Pressable} from 'react-native';
 
 export default function App() {
   const imagenExterno_react={uri:'https://cdn.ipadizate.com/2022/03/wallpaper-iPhone-SE-3-Negro.jpg?width=1200'}
@@ -13,12 +13,46 @@ export default function App() {
   return (
     //view = div, siempre tiene que estar adentro de uno 
     //dentro de los componentes van las propiedades
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>     
+    <SafeAreaView rstyle={styles.container}>
+      <View >     
         <StatusBar style="auto" />
 
         <ImageBackground source={imagenLocal_react} resizeMode="cover" style={styles.backgroundImage}>
-        <Text style={styles.Creditos}> Esta es la pagina oficial de                          Shirly y Maia {'\n'}</Text> 
+        <Text style={styles.Creditos}> Esta es la pagina oficial de                          Shirly y Maia {'\n'}</Text>
+
+
+        <Text style={styles.Consigna}> Ingrese los datos del egresado... {'\n'}</Text>
+
+        <Text> Nombre: {'\n'}</Text>
+
+        <TextInput
+
+          style={styles.input}   
+          placeholder= "Nombre..."
+        />
+        <Text> Telefono: {'\n'}</Text>
+
+        <TextInput
+          style={styles.input}   
+          placeholder= "Telefono"
+          keyboardType='number-pad'
+        />    
+
+        <Text> email: {'\n'}</Text>
+
+        <TextInput
+          style={styles.input}   
+          placeholder= "email"
+          keyboardType='email-address'
+        />
+
+        <Text> Clave: {'\n'}</Text>
+
+        <TextInput
+          style={styles.input}   
+          placeholder= "Clave"
+        />  
+
         <Text style={styles.text}> ¿Cual fue tu fiesta favotita de Bariloche? </Text> 
 
         <Button     
@@ -28,22 +62,20 @@ export default function App() {
         />
  
        
-         <TouchableOpacity style={styles.TouchableOpacity} onPress={handlePress}>     
-        
-            <Text>Semaforo</Text>     
-            </TouchableOpacity>
+         <TouchableOpacity onPress={handlePress}>     
+            <Text style={styles.Semaforo}> SEMAFORO </Text>     
+        </TouchableOpacity>
 
           <Button      
           title="Disfraces"    
           color='#f194ff'
           onPress={() => Alert.alert('Se ha presionado!')}    
         />
-      
-        <TextInput
-          style={styles.input}   
-          placeholder= "Otra..."
-        />
 
+        <Pressable onLongPress={() => Alert.alert('RE RATA!')} >
+          <Text style={styles.Semaforo}>ANIMAL</Text> 
+        </Pressable>
+      
         </ImageBackground> 
 
       </View>
@@ -71,17 +103,24 @@ const styles = StyleSheet.create({
   },
   input:{
     backgroundColor:'#ffffff',
-    borderWidth: 2,
+    borderWidth: 2
     
   },
   Creditos:{
     marginTop: -500,
     color:'#FFFFFF',
     textAlign: 'center',
-    fontSize : 20
+    fontSize : 17
   },
-  TouchableOpacity:{
-    color:'#f194ff'
-  
+  Consigna:{
+    color:'#FFFFFF',
+    fontSize : 15,
+    textAlign: 'center'
+  },
+  Semaforo:{
+    color:'#FFFFFF',
+    backgroundColor:'#f194ff',
+    fontSize : 15,
+    textAlign: 'center'
   }
 });
